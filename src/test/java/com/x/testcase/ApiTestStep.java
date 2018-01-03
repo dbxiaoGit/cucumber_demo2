5f6dcf311bfadffbf8a5379adbf81870301fca49
Derek <136187300@qq.com>
2018-01-04 01:34:22 +0800

    实现feature中的步骤

--- src/test/java/com/x/testcase/ApiTestStep.java
+++ src/test/java/com/x/testcase/ApiTestStep.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONPath;
 import com.x.common.HTTPUtil;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
+import cucumber.api.java.en.Given;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +15,25 @@ import org.slf4j.LoggerFactory;
 public class ApiTestStep extends HTTPUtil {
     private Logger logger = LoggerFactory.getLogger(getClass());
 
+    @Given("^指定请求url为(.+?)$")
+    public void setRequestUrl(String url) throws Throwable{
+        logger.info("setRequestUrl : {}",url);
+        this.uri = url;
+    }
+
+    @Given("^指定请求方法为(.+?)$")
+    public void setRequestMethod(String requestMethod) throws Throwable{
+        logger.info("setRequestMethod : {}",requestMethod);
+        this.requestMethod = requestMethod;
+    }
+
+    @Given("^初始化请求参数")
+    public void setRequestParam(String requestParam) throws Throwable{
+        logger.info("setRequestParam : {}",requestParam);
+        this.param = requestParam;
+    }
+
+    @Given("^修改参数(.+?)的值为(.+?)$")
     public void alterJsonBody(String jsonPath,String value) {
         logger.info("jsonPath:" + jsonPath);
         logger.info("修改前param:{}" , param);
@@ -22,6 +42,22 @@ public class ApiTestStep extends HTTPUtil {
         param = jsonObject.toJSONString();
         logger.info("修改后param:{}" , param);
     }
+
+    @Given("^执行请求")
+    public void excuteReuest() throws Throwable{
+        logger.info("excuteReuest");
+        sendRequest();
+    }
+
+    @Given("^检查jsonPath为(.+?)是否为(.+?)")
+    public void checkResponse(String jsonPath,String expectedValue) throws Throwable{
+        logger.info("checkResponse,jsonPath:{},expectedValue:{}",jsonPath,expectedValue);
+        com.alibaba.fastjson.JSONObject jsonObject = com.alibaba.fastjson.JSON.parseObject(result);
+        //setCheckPoint(jsonObject.,expectedValue);
+        check();
+    }
+
+
 
     @After("@api_test")
     public void testAfter(Scenario s){
